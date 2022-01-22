88bd67960a7f01cf7a1c1defc64caa58f95457e2
presidentielle2022 <mrbugsbenny@gmail.com>
2022-01-22 19:01:58 +0100

    a

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-window.alert("ok");
+function loadXMLDoc() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      window.alert(this);
+    }
+  };
+  xmlhttp.open("GET", "../candidats.xml", true);
+  xmlhttp.send();
+}
